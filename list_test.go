8b010c804081c29e16133883ef6b4556c9bc44f8
Gregorio Di Stefano <greg.distefano+github@gmail.com>
2016-11-05 00:45:01 +0100

    add basic file list and dir list tests

---
+++ list_test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDirsListing(t *testing.T) {
+	bs, keys := setupUp()
+	defer tearDown(bs)
+
+	dirListTests := []struct {
+		uploadFilepath       string
+		destinationDirectory string
+
+		deleteAfterTest bool
+		expectedError   interface{}
+		expectedOutput  []string
+	}{
+		{"", "", true, nil, []string{}},
+		{"testdata/*", "", true, nil, []string{"testdata",
+			"testdata/nested_1",
+			"testdata/nested_1/nested_nested_1",
+			"testdata/nested_1/nested_nested_1/nested_nested_nested_1",
+			"testdata/nested_2",
+			"testdata/nested_3",
+			"testdata/test_a",
+			"testdata/test_b",
+		}},
+		{"testdata/*", "abc", true, nil, []string{"testdata",
+			"abc/testdata/nested_1",
+			"abc/testdata/nested_1/nested_nested_1",
+			"abc/testdata/nested_1/nested_nested_1/nested_nested_nested_1",
+			"abc/testdata/nested_2",
+			"abc/testdata/nested_3",
+			"abc/testdata/test_a",
+			"abc/testdata/test_b",
+		}},
+	}
+
+	for _, e := range dirListTests {
+		if e.uploadFilepath != "" {
+			err := processUpload(bs, &keys, e.uploadFilepath, e.destinationDirectory)
+			assert.Nil(t, err)
+		}
+
+		dirsInBucket, err := getDirList(bs, &keys, "")
+		assert.Nil(t, err)
+		assert.EqualValues(t, e.expectedOutput, dirsInBucket)
+	}
+
+}
+
+func TestFileListing(t *testing.T) {
+	bs, keys := setupUp()
+	defer tearDown(bs)
+
+	dirListTests := []struct {
+		uploadFilepath       string
+		destinationDirectory string
+
+		deleteAfterTest bool
+		expectedError   interface{}
+		expectedOutput  []string
+	}{
+		{"", "", true, nil, nil},
+		{"testdata/*", "", true, nil, []string{
+			"testdata/nested_1/nested_nested_1/nested_nested_nested_1/testdata1",
+			"testdata/nested_1/nested_nested_1/testdata1",
+			"testdata/nested_1/testdata1",
+			"testdata/nested_2/testdata2",
+			"testdata/nested_3/testdata1",
+			"testdata/nested_3/testdata2",
+			"testdata/nested_3/testdata3",
+			"testdata/nested_3/testdata4",
+			"testdata/test_a/a",
+			"testdata/test_b/b",
+			"testdata/testdata1",
+			"testdata/testdata2",
+			"testdata/testdata3",
+			"testdata/testdata4",
+			"testdata/testdata5",
+			"testdata/testdata6",
+		}},
+	}
+
+	for _, e := range dirListTests {
+		if e.uploadFilepath != "" {
+			err := processUpload(bs, &keys, e.uploadFilepath, e.destinationDirectory)
+			assert.Nil(t, err)
+		}
+
+		filesInBucket, err := getFileList(bs, &keys, "")
+		fmt.Println("files: ", filesInBucket)
+		assert.Nil(t, err)
+		assert.EqualValues(t, e.expectedOutput, filesInBucket)
+	}
+
+}
